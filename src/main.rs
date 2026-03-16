0abcec52bace14bde81490d8604b3ee702926d9f
The_Fireplace <git@the-fireplace.dev>
2026-03-15 20:17:09 -0500

    Make use of let chains

--- src/main.rs
+++ src/main.rs
@@ -12,20 +12,20 @@ async fn main() -> std::io::Result<()> {
 pub fn configure(cfg: &mut web::ServiceConfig) {
     let mount_path = env::var("MOUNT_PATH");
     let serve_from = env::var("SERVE_FROM");
-    if let Ok(mount_path) = mount_path {
-        if let Ok(serve_from) = serve_from {
-            cfg.service(
-                actix_files::Files::new(&mount_path, serve_from)
-                    .guard(guard::Get())
-                    .show_files_listing(),
-            );
-        }
+    if let Ok(mount_path) = mount_path
+        && let Ok(serve_from) = serve_from
+    {
+        cfg.service(
+            actix_files::Files::new(&mount_path, serve_from)
+                .guard(guard::Get())
+                .show_files_listing(),
+        );
     }
 
-    if let Ok(allow_redirect) = env::var("REDIRECT_TO_HTTPS") {
-        if !allow_redirect.is_empty() {
-            cfg.default_service(web::route().to(https_redirect_handler));
-        }
+    if let Ok(allow_redirect) = env::var("REDIRECT_TO_HTTPS")
+        && !allow_redirect.is_empty()
+    {
+        cfg.default_service(web::route().to(https_redirect_handler));
     }
 }
 
